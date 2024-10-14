bb48ba9ec8c9747aab83f233829d8f7e60dc702a
Otajon Makxmasoliyev <111396286+OtajonMakxmasoliyev@users.noreply.github.com>
2024-10-14 18:16:57 +0500

    Update create_pdf.py

--- create_pdf.py
+++ create_pdf.py
@@ -519,7 +519,7 @@ def create_final_pdf(data):
                     "RIGHTPADDING",
                     (0, 0),
                     (-1, -1),
-                    0,
+                    8,
                 ),  # O'ngdan paddingni olib tashlash
                 ("TOPPADDING", (0, 0), (-1, -1), 10),
                 ("FONTSIZE", (0, 1), (-1, -1), 9),  # Body font size
